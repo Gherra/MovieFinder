86e59a8f383ecb49fd93f935b8addce8976da479
Raman Kumar <raman16894@gmail.com>
2025-10-18 16:24:03 -0700

    Add instructions to ApiConfig.example.kt file

--- app/src/main/java/com/ramankumar/moviefinder/api/ApiConfig.example.kt
+++ app/src/main/java/com/ramankumar/moviefinder/api/ApiConfig.example.kt
@@ -2,5 +2,7 @@ package com.ramankumar.moviefinder.api
 
 object ApiConfig {
     // REPLACE WITH YOUR ACTUAL API KEY FROM https://www.themoviedb.org/
+    // and then rename the file from ApiConfig.example.kt to ApiConfig.kt
+
     const val API_KEY = "YOUR_TMDB_API_KEY_HERE"
 }
