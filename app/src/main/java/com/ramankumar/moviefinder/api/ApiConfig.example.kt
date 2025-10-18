8bf9c74ef5f9a0ac79ca52b01fba3eb16a2ac95b
Raman Kumar <raman16894@gmail.com>
2025-10-18 16:06:08 -0700

    Add ApiConfig.example.kt for teammates to use

---
+++ app/src/main/java/com/ramankumar/moviefinder/api/ApiConfig.example.kt
@@ -0,0 +1,6 @@
+package com.ramankumar.moviefinder.api
+
+object ApiConfig {
+    // REPLACE WITH YOUR ACTUAL API KEY FROM https://www.themoviedb.org/
+    const val API_KEY = "YOUR_TMDB_API_KEY_HERE"
+}
