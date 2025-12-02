3c792413a19348dbf6d43439d449255988883d3d
Nicholas Tam <nta59@sfu.ca>
2025-12-01 19:46:46 -0800

    FEAT [AuthRepository] basic login, logout, register with email and password.

--- app/src/main/java/com/ramankumar/moviefinder/data/repository/auth/AuthRepository.kt
+++ app/src/main/java/com/ramankumar/moviefinder/data/repository/auth/AuthRepository.kt
@@ -1,7 +1,37 @@
 package com.ramankumar.moviefinder.data.repository.auth
 
-class AuthRepository {
-    /*
-    TODO: Create repository to handle calls and tokens with Firebase from user
-     */
+import com.google.firebase.auth.FirebaseAuth
+import com.ramankumar.moviefinder.model.auth.AuthResult
+import kotlinx.coroutines.tasks.await
+
+class AuthRepository(
+    private val auth: FirebaseAuth = FirebaseAuth.getInstance()
+) {
+
+    suspend fun login(email: String, password: String): AuthResult{
+        return try{
+            auth.signInWithEmailAndPassword(email, password).await()
+            AuthResult.Success
+        } catch (e: Exception){
+            AuthResult.Failure(e.message)
+        }
+    }
+
+    suspend fun register(email: String, password: String): AuthResult{
+        return try {
+            auth.createUserWithEmailAndPassword(email,password).await()
+            AuthResult.Success
+        } catch (e: Exception){
+            AuthResult.Failure(e.message)
+        }
+    }
+
+    fun logout(){
+        auth.signOut()
+    }
+
+    fun currentUser() = auth.currentUser
+
+
+
 }
